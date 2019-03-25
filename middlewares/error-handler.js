0562bf78e433d4758a4e0760e41e7af24960b0df
Efabien <enone@protonmail.com>
2019-03-25 16:36:01 +0300

    llee

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -4,7 +4,7 @@ module.exports = class ErrorHandler {
   }
 
   handler(err, req, res, next) {
-    console.log('Error during request processing', err);
+    console.log('Error during request processing : ', err);
     res.status(error.statusCode || 500).send(
       { message: error.message || error.errorMessage }
     );
